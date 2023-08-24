87a3649afb790657e392eb0426720b39dffce0a5
Choi-Yuchan <cor330@gmail.com>
2023-08-24 12:39:03 +0900

    create a repository

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["react"],
+  extends: ["eslint:recommended", "plugin:react/jsx-runtime"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  ignorePatterns: ["build", "dist", "public"],
+};
