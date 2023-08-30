109383637d10aac17afcaa66b69b95fe17dbbdc1
Choi-Yuchan <cor330@gmail.com>
2023-08-30 11:47:26 +0900

    tracking screenY and add a state when topbutton is shown.

--- src/components/ToTopBtn.jsx
+++ src/components/ToTopBtn.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { styled } from "styled-components";
 
 const StyledButton = styled.button`
@@ -14,17 +15,35 @@ const BtnContainer = styled.div`
   position: sticky;
   bottom: 1rem;
   width: 100%;
-  display: flex;
+  display: ${({ $isShow }) => ($isShow ? "flex" : "none")};
   justify-content: flex-end;
   margin-bottom: 1rem;
 `;
 
 const ToTopBtn = () => {
+  const [isShow, setIsShow] = useState(false);
   const moveToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+    }
+  };
+  useEffect(() => {
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <BtnContainer>
+    <BtnContainer $isShow={isShow}>
       <StyledButton onClick={() => moveToTop()}>TOP</StyledButton>
     </BtnContainer>
   );
