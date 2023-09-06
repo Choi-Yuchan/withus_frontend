48e46ad72e04bdfc66d30b4118291ce5fbda4e29
[heosol3-3] <[samz3@naver.com]>
2023-09-06 11:57:14 +0900

    signup, mypage modify

--- src/page/MyPage.jsx
+++ src/page/MyPage.jsx
@@ -1,36 +1,211 @@
-import Button from "components/Button";
 import { Footer } from "components/Footer";
 import { Header } from "components/Header";
 import React, { useState } from "react";
+import styled from "styled-components";
+import DaumPostcode from "react-daum-postcode";
+
 
 const MyPage = () => {
+  // 사용자 정보 상태 관리
   const [userInfo, setUserInfo] = useState({
-    username: "사용자 이름",
-    number: "010-0000-0000",
-    address: "korea, seoul, jong-no 336, 201",
+    name: "Your Name",
+    id: "Your Id",
+    phoneNumber: "123-456-7890",
+    address: "Address",
   });
-  const handleEdit = () => {
+
+  // 수정 모드 상태 관리
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  // 입력 필드 값 변경 핸들러
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
     setUserInfo({
-      username: "new name",
-      number: "new number",
-      address: "new address",
+      ...userInfo,
+      [name]: value,
+    });
+  };
+
+  // 저장 버튼 클릭 핸들러
+  const handleSaveClick = () => {
+    setIsEditMode(false); // 수정 모드 종료
+  };
+
+  const [popup, setPopup] = useState(false);
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+
+    let extraAddress = "";
+
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+
+    console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+    console.log(data.zonecode); // 우편번호
+    setUserInfo({
+      ...userInfo,
+      address: fullAddress,
     });
   };
   return (
     <div>
       <Header />
-      <div>
-        <h2>마이 페이지</h2>
+      <MyPageContainer>
+        <h2>My Page</h2>
+        <UserInfo>
+          <UserInfoItem>
+            <label>이름:</label>
+            {isEditMode ? (
+              <input
+                type="text"
+                name="name"
+                value={userInfo.name}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{userInfo.name}</span>
+            )}
+          </UserInfoItem>
+          <UserInfoItem>
+            <label>아이디:</label>
+            <span>{userInfo.id}</span>
+          </UserInfoItem>
+          <UserInfoItem>
+            <label>전화번호:</label>
+            {isEditMode ? (
+              <input
+                type="text"
+                name="phoneNumber"
+                value={userInfo.phoneNumber}
+                onChange={handleInputChange}
+              />
+            ) : (
+              <span>{userInfo.phoneNumber}</span>
+            )}
+          </UserInfoItem>
+          <UserInfoItem>
+            <label>주소</label>
+            {isEditMode ? (
+              <input
+                type="text"
+                name="address"
+                value={userInfo.address}
+                readOnly
+                onClick={() => setPopup(true)}
+                onChange={handleInputChange}
+                required
+              />
+            ) : (
+              <span>{userInfo.address}</span>
+            )}
+            <PopupContainer>
+                {popup && (
+                  <DaumPostcode
+                    onComplete={handleComplete}
+                    autoClose={true}
+                    animation={true}
+                    style={{
+                      position: "absolute",
+                      width: "400px",
+                      height: "100%",
+                      border: "1px solid rgba(100,100,100,0.5)",
+                      paddingBottom: "0",
+                      zIndex: 100,
+                    }}
+                  />
+                )}
+              </PopupContainer>
+          </UserInfoItem>
+        </UserInfo>
         <div>
-          <p>사용자 이름: {userInfo.username}</p>
-          <p>전화번호:{userInfo.number}</p>
-          <p>주소:{userInfo.address}</p>
+          <EditButton onClick={() => setIsEditMode(true)}>수정</EditButton>
+          {isEditMode && (
+            <SaveButton onClick={handleSaveClick}>저장</SaveButton>
+          )}
         </div>
-        <Button title={"정보수정"} onClick={handleEdit} />
-      </div>
+      </MyPageContainer>
       <Footer />
     </div>
   );
 };
+
+const MyPageContainer = styled.div`
+  text-align: center;
+  padding: 10rem 5rem 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 10rem;
+  > h2 {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-bottom: 5rem;
+  }
+`;
+
+const UserInfo = styled.div`
+  margin-top: 1rem;
+  width: 30rem;
+  display: flex;
+  flex-direction: column;
+  > div {
+    padding-bottom: 1.5rem;
+    border-bottom: 1px solid rgba(150, 150, 150, 0.5);
+    margin-bottom: 1.5rem;
+  }
+`;
+
+const UserInfoItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+
+  label {
+    font-weight: bold;
+    margin-right: 1rem;
+  }
+  input {
+    padding: 0.2rem;
+  }
+`;
+
+const EditButton = styled.button`
+  margin-top: 1rem;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+`;
+
+const SaveButton = styled.button`
+  margin-top: 1rem;
+  background-color: #28a745;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  margin-left: 0.5rem;
+`;
+
+const PopupContainer = styled.div`
+  position: fixed;
+  right: 5rem;
+  bottom: 5rem;
+  width: 400px;
+  height: 500px;
+`;
 
 export default MyPage;
--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -18,14 +18,71 @@ const SignUp = () => {
   });
 
   const [popup, setPopup] = useState(false);
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=.]).{8,15}$/;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    let updatedValue = value;
+
+    const errors = { ...formErrors };
+
+    if (name === "name") {
+      if (!value) {
+        errors[name] = "이름을 입력하세요.";
+      } else {
+        delete errors[name];
+      }
+    }
+    if (name === "id") {
+      // 아이디 유효성 검사
+      if (!value) {
+        errors[name] = "아이디를 입력하세요.";
+      } else if (!/^[a-z0-9]+$/.test(value)) {
+        errors[name] = "아이디는 소문자와 숫자로만 허용됩니다.";
+      } else {
+        delete errors[name];
+      }
+    }
+    if (name === "password") {
+      // 비밀번호 유효성 검사
+      if (!passwordRegex.test(value)) {
+        errors[name] =
+          "비밀번호는 최소 8자 이상 15자미만, 대소문자, 숫자, 특수 문자를 포함해야 합니다.";
+      } else {
+        delete errors[name];
+      }
+    }
+    if (name === "confirmPassword") {
+      // 비밀번호 확인 유효성 검사
+      if (value !== formData.password) {
+        errors[name] = "비밀번호와 일치하지 않습니다.";
+      } else {
+        delete errors[name];
+      }
+    }
+    if (name === "phoneNumber") {
+      // 휴대폰 번호 입력 시 '-' 추가
+      updatedValue = updatedValue
+        .replace(/[^0-9]/g, "") // 숫자 이외의 문자 제거
+        .slice(0, 11) // 8글자까지만 유효
+        .replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3"); // 번호 형식 변경
+    } else if (name === "birthday") {
+      // 생년월일 숫자로만 받고 '-' 추가
+      updatedValue = updatedValue
+        .replace(/[^0-9]/g, "") // 숫자 이외의 문자 제거
+        .slice(0, 8) // 8글자까지만 유효
+        .replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3"); // 년월일 형식 변경
+    }
+
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: updatedValue,
     });
+    setFormErrors(errors);
   };
+
+  const [formErrors, setFormErrors] = useState({});
 
   const handleComplete = (data) => {
     let fullAddress = data.address;
@@ -46,12 +103,21 @@ const SignUp = () => {
     }
 
     console.log(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
-
-    console.log(data.zonecode); // 우편번호'
+    console.log(data.zonecode); // 우편번호
+    setFormData({
+      ...formData,
+      address: fullAddress,
+    });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const errors = { ...formErrors };
+
+    if (Object.keys(errors).length > 0) {
+      return;
+    }
 
     try {
       // 회원가입 API 호출
@@ -69,96 +135,115 @@ const SignUp = () => {
         <h2>회원가입</h2>
         <SignUpForm onSubmit={handleSubmit}>
           <div>
-            <InputLabel>이름</InputLabel>
-            <Input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
+            <div>
+              <InputLabel>이름</InputLabel>
+              <Input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {formErrors.name && <Error>{formErrors.name}</Error>}
           </div>
           <div>
-          <InputLabel htmlFor="username">아이디</InputLabel>
-            <Input
-              type="text"
-              name="id"
-              value={formData.id}
-              onChange={handleChange}
-              required
-            />
+            <div>
+              <InputLabel htmlFor="username">아이디</InputLabel>
+              <Input
+                type="text"
+                name="id"
+                value={formData.id}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {formErrors.id && <Error>{formErrors.id}</Error>}
           </div>
           <div>
-          <InputLabel htmlFor="password">비밀번호</InputLabel>
-            <Input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
+            <div>
+              <InputLabel htmlFor="password">비밀번호</InputLabel>
+              <Input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {formErrors.password && <Error>{formErrors.password}</Error>}
           </div>
           <div>
-          <InputLabel htmlFor="confirmPassword">비밀번호 확인</InputLabel>
-            <Input
-              type="password"
-              name="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleChange}
-              required
-            />
+            <div>
+              <InputLabel htmlFor="confirmPassword">비밀번호 확인</InputLabel>
+              <Input
+                type="password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {formErrors.confirmPassword && (
+              <Error>{formErrors.confirmPassword}</Error>
+            )}
           </div>
           <div>
-            <InputLabel htmlFor="phoneNumber">휴대폰 번호</InputLabel>
-            <Input
-              type="tel"
-              name="phoneNumber"
-              value={formData.phoneNumber}
-              onChange={handleChange}
-              required
-            />
+            <div>
+              <InputLabel htmlFor="phoneNumber">휴대폰 번호</InputLabel>
+              <Input
+                type="tel"
+                name="phoneNumber"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
           <div>
-            <InputLabel htmlFor="address">주소</InputLabel>
-            <Input
-              type="text"
-              name="address"
-              value={formData.address}
-              readOnly
-              onClick={() => setPopup(true)}
-              required
-            />
-            <PopupContainer>
-              {popup && (
-                <DaumPostcode
-                  onComplete={handleComplete}
-                  autoClose={true}
-                  animation={true}
-                  style={{
-                    position: "absolute",
-                    right: "-27rem",
-                    bottom: "20rem",
-                    width: "400px",
-                    height: "100%",
-                    border: "1px solid rgba(100,100,100,0.5)",
-                    paddingBottom: "0",
-                    zIndex: 100,
-                  }}
-                />
-              )}
-            </PopupContainer>
+            <div>
+              <InputLabel htmlFor="address">주소</InputLabel>
+              <Input
+                type="text"
+                name="address"
+                value={formData.address}
+                readOnly
+                onClick={() => setPopup(true)}
+                required
+              />
+
+              <PopupContainer>
+                {popup && (
+                  <DaumPostcode
+                    onComplete={handleComplete}
+                    autoClose={true}
+                    animation={true}
+                    style={{
+                      position: "absolute",
+                      width: "400px",
+                      height: "100%",
+                      border: "1px solid rgba(100,100,100,0.5)",
+                      paddingBottom: "0",
+                      zIndex: 100,
+                    }}
+                  />
+                )}
+              </PopupContainer>
+            </div>
           </div>
           <div>
-            <InputLabel>생년월일</InputLabel>
-            <Input
-              type="text"
-              name="birthday"
-              value={formData.birthday}
-              onChange={handleChange}
-              required
-            />
+            <div>
+              <InputLabel>생년월일</InputLabel>
+              <Input
+                type="text"
+                name="birthday"
+                value={formData.birthday}
+                onChange={handleChange}
+                required
+              />
+            </div>
           </div>
-          <Button type="submit" title={"가입완료"} href={"/"} />
+          <Button title={"가입완료"} href={"/"} />
         </SignUpForm>
       </SignUpContainer>
       <Footer />
@@ -180,26 +265,30 @@ const SignUpForm = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
-  div {
+  > div {
+    display: flex;
+    flex-direction: column;
+    justfy-content: space-around;
+    margin: 1.5rem 0 0;
+    padding-bottom: 1.5rem;
+    border-bottom: 1px solid rgba(150, 150, 150, 0.3);
+  }
+  > div > div {
     display: flex;
     flex-direction: row;
     width: 30rem;
     justify-content: space-between;
     align-items: center;
     text-align: left;
-    margin: 1.5rem 0 0;
-    padding-bottom: 1.5rem;
-    border-bottom: 1px solid rgba(120, 120, 120, 0.5);
   }
-  div:last-of-type {
+  >div:last-of-type {
     border: none;
-    margin-bottom: 2rem;
+    margin-bottom: 4rem;
   }
 `;
 
 const InputLabel = styled.label`
-  text-align: left
-  padding: 0.5rem 5rem;
+  text-align: left;
 `;
 
 const Input = styled.input`
@@ -211,8 +300,17 @@ const Input = styled.input`
 
 const PopupContainer = styled.div`
   position: fixed;
+  right: 0rem;
+  bottom: 5rem;
   width: 400px;
   height: 500px;
+`;
+
+const Error = styled.span`
+  color: red;
+  font-size: 0.8rem;
+  margin-top: 0.5rem;
+  text-align: right;
 `;
 
 export default SignUp;
